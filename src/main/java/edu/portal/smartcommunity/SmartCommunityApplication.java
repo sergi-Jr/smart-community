e3db33ed3585ab9aac9d660eea17da23a23a6599
sergi-jr <burashnikov.sergei1@mail.ru>
2025-01-13 16:42:25 +0300

    add exception with handler

--- src/main/java/edu/portal/smartcommunity/SmartCommunityApplication.java
+++ src/main/java/edu/portal/smartcommunity/SmartCommunityApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Locale;
-
 @SpringBootApplication
 public class SmartCommunityApplication {
 
---
+++ src/main/java/edu/portal/smartcommunity/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.portal.smartcommunity.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
