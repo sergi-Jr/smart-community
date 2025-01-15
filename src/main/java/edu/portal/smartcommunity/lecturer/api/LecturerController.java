8be7dc70753ab4cf3b150d0fde46b5bb8891212a
sergi-jr <burashnikov.sergei1@mail.ru>
2025-01-15 19:33:06 +0300

    add valid annotation in some methods

--- src/main/java/edu/portal/smartcommunity/lecturer/api/LecturerController.java
+++ src/main/java/edu/portal/smartcommunity/lecturer/api/LecturerController.java
@@ -4,9 +4,18 @@ import edu.portal.smartcommunity.lecturer.dal.LecturerService;
 import edu.portal.smartcommunity.lecturer.dto.LecturerCreateDTO;
 import edu.portal.smartcommunity.lecturer.dto.LecturerDTO;
 import edu.portal.smartcommunity.lecturer.dto.LecturerUpdateDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,12 +38,12 @@ public class LecturerController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public LecturerDTO create(@RequestBody LecturerCreateDTO dto) {
+    public LecturerDTO create(@RequestBody @Valid LecturerCreateDTO dto) {
         return lecturerService.create(dto);
     }
 
     @PatchMapping(path = "/{id}")
-    public LecturerDTO update(@PathVariable UUID id, @RequestBody LecturerUpdateDTO dto) {
+    public LecturerDTO update(@PathVariable UUID id, @RequestBody @Valid LecturerUpdateDTO dto) {
         return lecturerService.update(id, dto);
     }
 
