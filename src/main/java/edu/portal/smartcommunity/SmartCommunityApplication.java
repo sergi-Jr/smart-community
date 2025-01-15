2b70dbaa7c85e512596ba99d8f76ac4fe692402b
sergi-jr <burashnikov.sergei1@mail.ru>
2025-01-15 19:30:32 +0300

    autochange into CRLF

--- src/main/java/edu/portal/smartcommunity/SmartCommunityApplication.java
+++ src/main/java/edu/portal/smartcommunity/SmartCommunityApplication.java
@@ -4,16 +4,17 @@ import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SmartCommunityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCommunityApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartCommunityApplication.class, args);
-	}
-
-	@Bean
-	public static Faker getFaker() {
-		return new Faker();
-	}
+    @Bean
+    public static Faker getFaker() {
+        return new Faker();
+    }
 }
