afaa55d2a923246065b001e06b999d7deee314d9
sergi-jr <burashnikov.sergei1@mail.ru>
2025-01-17 16:47:58 +0300

    add email search in lecturer entity

--- src/main/java/edu/portal/smartcommunity/lecturer/dal/LecturerRepository.java
+++ src/main/java/edu/portal/smartcommunity/lecturer/dal/LecturerRepository.java
@@ -4,8 +4,10 @@ import edu.portal.smartcommunity.lecturer.model.Lecturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LecturerRepository extends JpaRepository<Lecturer, UUID> {
+    Optional<Lecturer> findByEmail(String email);
 }
